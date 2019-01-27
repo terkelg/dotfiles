8bf8e5639990ca20cc979eb6c60281c1f8e08a5f
Terkel Gjervig <terkel@terkel.com>
2019-01-27 20:47:49 +0100

    added comments

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-npm install -g http-server
-npm install -g ndb
+npm install -g http-server  # serve all everything
+npm install -g ndb          # improved debugging experience for Node.js
+npm install -g neovim       # node neovim client
+npm install -g typescript   # some people like types in their JS
